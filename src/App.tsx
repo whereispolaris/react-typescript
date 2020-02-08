15d5e5924d411d1d24b3e986267e4f49117b1d07
Santiago Enciso <whereispolaris@gmail.com>
2020-02-08 15:10:38 -0600

    Defined initialized different types and added values to each type.

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Initialize type and value
+	let firstValue: string = 'Santiago';
+	let secondValue: number = 10;
+	let thirdValue: boolean = true;
+	let fourthValue: number[] = [2, 3, 56];
+	let fifthValue: Array<number> = [2, 3, 56];
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					The value {firstValue} is of {typeof firstValue} type!
+				</p>
+				<p>
+					The value {secondValue} is of {typeof secondValue} type!
+				</p>
+				<p>
+					The value {thirdValue} is of {typeof thirdValue} type!
+				</p>
+				<p>
+					The value {fourthValue} is of {typeof fourthValue} type!
+				</p>
+				<p>
+					The value {fifthValue} is of {typeof fifthValue} type!
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
